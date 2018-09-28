e7bbf4f014926025c04d78b2276eb0931458686a
Ivo Pugliese <elsoft74@gmail.com>
2018-09-28 23:29:50 +0200

    Aggiunti i comandi per interrogare il db. Va corretto il valore restituito in detection

--- Esame/src/esame/server/Server.java
+++ Esame/src/esame/server/Server.java
@@ -31,7 +31,9 @@ import java.net.URLDecoder;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -92,7 +94,8 @@ public class Server {
 //            }
                 DiskFileItemFactory d = new DiskFileItemFactory();      
                 OutputStream os = t.getResponseBody();               
-                    
+                JSONObject out = new JSONObject();
+                out.put("status", false);    
                 try {
                     ServletFileUpload up = new ServletFileUpload(d);
                     List<FileItem> result = up.parseRequest(new RequestContext() {
@@ -144,16 +147,16 @@ public class Server {
                         if (!(fileName.compareTo("")==0||json==null)){ // se sono stati valorizzati, quindi ho ricevuto correttamente i dati
                             inserisciDb(json); // inserisco nel db locale
                         }
-                        //System.out.println("File-Item: " + fi.getFieldName() + " = " + fi.getName()+" size="+fi.getSize());
                     }
+                    out.put("status",true);
                     t.sendResponseHeaders(200, 0);
-                    os.write("OK\r\n".getBytes());
+                    os.write(out.toJSONString().getBytes());
                     os.close();
 
                 } catch (Exception e) {
                     e.printStackTrace();
                     t.sendResponseHeaders(404, 0);
-                    os.write("KO\r\n".getBytes());
+                    os.write(out.toJSONString().getBytes());
                     os.close();
                 } 
 
@@ -164,7 +167,6 @@ public class Server {
             String fileName="";
             String warningLevel="";
             String time="";
-            //System.out.println(json.keySet().toString());
             if (json.containsKey("riconoscimento")) {
                 
                 JSONObject tmp= (JSONObject) json.get("riconoscimento");
@@ -210,66 +212,69 @@ public class Server {
         @Override
         public void handle(HttpExchange t) throws IOException {
             HashMap<String, Object> params = new HashMap();
-                    parseQuery(t.getRequestURI().getQuery(),params);
+            parseQuery(t.getRequestURI().getQuery(),params);
 
-                 OutputStream os = t.getResponseBody();
-                JSONObject out = new JSONObject();
-                out.put("status", false);
+            OutputStream os = t.getResponseBody();
+            JSONObject out = new JSONObject();
+            out.put("status", false);
+            try {
+                t.getResponseHeaders().add("Content-type", "text/plain");
+                String command=(String) params.get("command");
+                int id=-1; // un valore non valido, ma ci fa comodo così. Se nell'URL id fosse impostato -1 verrebbe sovrascritto
                 try {
-                    t.getResponseHeaders().add("Content-type", "text/plain");
-                    String command=(String) params.get("command");
-                    int id=Integer.parseInt((String) params.get("id"));
-                   
-                    /*
-                    * dopo aver acquisito il comando da eseguire
-                    * e l'eventuale paramentro
-                    * si esegue il metodo opportuno
-                    * i metodi restituiranno sempre un JSON nel formato
-                    * {"
-                        id":id,
-                        "data":{
-                            ... i dati della decodifica
-                        }
-                    }
-                    */
-                    
-                    if (command!=null){
-                        
-                        switch(command){ 
-                            case "listNew": // chiede al db i soli contenuti con flag new a 1
-                                out=interrogaDB(command);
-                                break;
-                            case "listAll": // chiede al db tutti i contenuti
-                                out=interrogaDB(command);
-                                break;
-                            case "read": // chiede al db il contentuo con id=...
-                                out=interrogaDB(command,id);
-                                break;
-                            case "update": // imposta lo stato di id=... a letto
-                                out=updateDB(id);
-                                break;
-                            case "getFile": // chiede il file associato a id=...
-                                out=interrogaDB(command,id);
-                                break;
-                            default: // restituiamo un errore
-                                break;
-                        }
-                    } else {
-                        throw new Exception("Comando non valido"); // per inviare all'altro capo delal connessione lo stato d'errore,
-                                                                   // sollevo un eccezione che verrà gestita nel catch
-                                                                   // così riutilizziamo la generazione dello stato 404 e del KO come messaggio di stato
-                    }
-                    System.out.println("comando: " + command +" id: "+id);
-                    t.sendResponseHeaders(200, 0);
-                    os.write(out.toJSONString().getBytes());
-                    os.close();
+                    id=Integer.parseInt((String) params.get("id"));
+                } catch(Exception ex){
+                    ; // non mi interessa fare nulla
+                }
 
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    t.sendResponseHeaders(404, 0);
-                    os.write(out.toJSONString().getBytes());
-                    os.close();
-                } 
+                /*
+                * dopo aver acquisito il comando da eseguire
+                * e l'eventuale paramentro
+                * si esegue il metodo opportuno
+                * i metodi restituiranno sempre un JSON nel formato
+                * {"
+                    id":id,
+                    "results":[
+                        ... array con i dati della decodifica
+                    ]
+                }
+                */
+
+                if (command!=null){
+
+                    switch(command){ 
+                        case "update": // imposta lo stato di id=... a letto
+                            out=updateDB(id);
+                            break;
+                        case "getFile": // chiede il file associato a id=...
+                            out=recuperaFile(id);
+                            break;
+                        case "listNew": // chiede al db i soli contenuti con flag new a 1
+                        
+                        case "listAll": // chiede al db tutti i contenuti
+                        
+                        case "read": // chiede al db il contentuo con id=...
+                            out=interrogaDB(command,id);
+                            break;
+                        
+                        default: // restituiamo un errore
+                            break;
+                    }
+                } else {
+                    throw new Exception("Comando non valido"); // per inviare all'altro capo delal connessione lo stato d'errore,
+                                                               // sollevo un eccezione che verrà gestita nel catch
+                                                               // così riutilizziamo la generazione dello stato 404 e del KO come messaggio di stato
+                }
+                t.sendResponseHeaders(200, 0);
+                os.write(out.toJSONString().getBytes());
+                os.close();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                t.sendResponseHeaders(404, 0);
+                os.write(out.toJSONString().getBytes());
+                os.close();
+            } 
 
 
         }
@@ -278,71 +283,97 @@ public class Server {
         public static void parseQuery(String query, Map<String, 
 	Object> parameters) throws UnsupportedEncodingException {
 
-         if (query != null) {
-                 String pairs[] = query.split("[&]");
-                 for (String pair : pairs) {
-                          String param[] = pair.split("[=]");
-                          String key = null;
-                          String value = null;
-                          if (param.length > 0) {
-                          key = URLDecoder.decode(param[0], 
-                          	System.getProperty("file.encoding"));
-                          }
+        if (query != null) {
+                String pairs[] = query.split("[&]");
+                for (String pair : pairs) {
+                        String param[] = pair.split("[=]");
+                        String key = null;
+                        String value = null;
+                        if (param.length > 0) {
+                        key = URLDecoder.decode(param[0], 
+                              System.getProperty("file.encoding"));
+                        }
 
-                          if (param.length > 1) {
-                                   value = URLDecoder.decode(param[1], 
-                                   System.getProperty("file.encoding"));
-                          }
+                        if (param.length > 1) {
+                                 value = URLDecoder.decode(param[1], 
+                                 System.getProperty("file.encoding"));
+                        }
 
-                          if (parameters.containsKey(key)) {
-                                   Object obj = parameters.get(key);
-                                   if (obj instanceof List<?>) {
-                                            List<String> values = (List<String>) obj;
-                                            values.add(value);
+                        if (parameters.containsKey(key)) {
+                                 Object obj = parameters.get(key);
+                                 if (obj instanceof List<?>) {
+                                          List<String> values = (List<String>) obj;
+                                          values.add(value);
 
-                                   } else if (obj instanceof String) {
-                                            List<String> values = new ArrayList<String>();
-                                            values.add((String) obj);
-                                            values.add(value);
-                                            parameters.put(key, values);
-                                            
-                                   }
-                          } else {
-                                   parameters.put(key, value);
-                          }
-                 }
+                                 } else if (obj instanceof String) {
+                                          List<String> values = new ArrayList<String>();
+                                          values.add((String) obj);
+                                          values.add(value);
+                                          parameters.put(key, values);
+
+                                 }
+                        } else {
+                                 parameters.put(key, value);
+                        }
+               }
          }
 }
         
-        static Map<String, String> getParameters(HttpExchange httpExchange) throws IOException {
-            Map<String, String> parameters = new HashMap<>();
-            InputStream inputStream = httpExchange.getRequestBody();
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            byte[] buffer = new byte[2048];
-            int read = 0;
-            while ((read = inputStream.read(buffer)) != -1) {
-              byteArrayOutputStream.write(buffer, 0, read);
-            }
-            String[] keyValuePairs = byteArrayOutputStream.toString().split("&");
-            for (String keyValuePair : keyValuePairs) {
-              String[] keyValue = keyValuePair.split("=");
-              if (keyValue.length != 2) {
-                continue;
-              }
-              parameters.put(keyValue[0], keyValue[1]);
-            }
-            return parameters;
-          }
+//        static Map<String, String> getParameters(HttpExchange httpExchange) throws IOException {
+//            Map<String, String> parameters = new HashMap<>();
+//            InputStream inputStream = httpExchange.getRequestBody();
+//            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+//            byte[] buffer = new byte[2048];
+//            int read = 0;
+//            while ((read = inputStream.read(buffer)) != -1) {
+//              byteArrayOutputStream.write(buffer, 0, read);
+//            }
+//            String[] keyValuePairs = byteArrayOutputStream.toString().split("&");
+//            for (String keyValuePair : keyValuePairs) {
+//              String[] keyValue = keyValuePair.split("=");
+//              if (keyValue.length != 2) {
+//                continue;
+//              }
+//              parameters.put(keyValue[0], keyValue[1]);
+//            }
+//            return parameters;
+//          }
 
 
-        private JSONObject interrogaDB(String command, int id) {
+        private JSONObject interrogaDB(String command, int id) throws SQLException {
             JSONObject out=new JSONObject();
-            out.put("status", true);
-            return out;
-        }
-        private JSONObject interrogaDB(String command) {
-            JSONObject out=new JSONObject();
-            out.put("status", true);
+            String query="SELECT * FROM ricezioni"; // seleziona tutti i contenuti
+            switch(command){ 
+                case "listNew": // chiede al db i soli contenuti con flag new a 1
+                    query+=" WHERE `new`= 1"; // aggiunge la clausola WHERE che imposta un filtro sul valore della colonna new. 1 vuol dire che non è stato ancora marcato come letto
+                    break;
+                case "read":
+                    query+=" WHERE `id`= ?"; // aggiunge la clausola WHERE che imposta un filtro sul valore della colonna id
+                    break;
+                case "listAll": // chiede al db tutti i contenuti
+                    break;
+            }
+            query+=" ORDER BY `time` ASC, `warning` DESC"; // ordina i risultati dal più vecchio ed in base al walore di WARNING
+            Connection conn=connect();
+            PreparedStatement stmt=conn.prepareStatement(query);
+            if (id!=-1){
+                stmt.setInt(1, id);
+            }
+            
+            ResultSet rs=stmt.executeQuery(); // esegue la query e riceve i risultati
+            ArrayList risultati= new ArrayList();
+            while (rs.next()){ // scorre i risultati
+                JSONObject tmp=new JSONObject(); // oggetto temporaneao dove memorizzare i risultati
+                tmp.put("id", rs.getInt("id"));
+                tmp.put("warning", rs.getString("warning"));
+                tmp.put("file", rs.getString("file"));
+                tmp.put("time", rs.getString("time"));
+                tmp.put("new", rs.getInt("new"));
+                tmp.put("detection", rs.getString("json"));
+                risultati.add(tmp); // accoda il risultato
+            }
+            out.put("status", true); // imposto true per confermare che tutto è andato bebe
+            out.put("results", risultati);
             return out;
         }
         
@@ -352,7 +383,7 @@ public class Server {
          */ 
 
         private JSONObject updateDB(int id) throws SQLException {
-             Connection conn=connect();
+            Connection conn=connect();
             JSONObject out=new JSONObject();
             out.put("status", false);
             if (conn!=null){
@@ -366,6 +397,10 @@ public class Server {
                 throw new SQLException("Impossibile aggiornare");
             }
             return out;
+        }
+
+        private JSONObject recuperaFile(int id) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         }
 
     }
